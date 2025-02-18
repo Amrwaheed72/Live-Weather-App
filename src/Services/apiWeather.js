8210849aae10bde1334ba72f750dbe7412872ebe
Amr Waheed <amrmohamed877@gmail.com>
2025-02-18 21:43:56 +0200

    ..

--- src/Services/apiWeather.js
+++ src/Services/apiWeather.js
@@ -1,6 +1,6 @@
 
 export async function fetchWeather(city) {
-    const API_KEY = "b0c056d3d14875fbd14f9f02f2001d25";
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;;
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     const response = await fetch(URL);
